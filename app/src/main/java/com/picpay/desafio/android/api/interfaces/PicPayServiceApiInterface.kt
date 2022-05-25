5aba3075b05ba3785bea3a02a42bf8616439fd9a
Danilo - Micro de Desenv <lamarca.danilo@gmail.com>
2022-05-25 13:45:21 -0300

    retirado ojetos nao usados, que foram reorganizados em novos diretorios + adequado nome e pacote da interface usada pelo retrofit

--- app/src/main/java/com/picpay/desafio/android/api/interfaces/PicPayServiceApiInterface.kt
+++ app/src/main/java/com/picpay/desafio/android/api/interfaces/PicPayServiceApiInterface.kt
@@ -1,4 +1,10 @@
 package com.picpay.desafio.android.api.interfaces
 
+import com.picpay.desafio.provider.model.User
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface PicPayServiceApiInterface {
+    @GET("users")
+    fun getUsers(): Call<List<User>>
 }
--- app/src/main/java/com/picpay/desafio/android/repository/PicPayRepository.kt
+++ app/src/main/java/com/picpay/desafio/android/repository/PicPayRepository.kt
@@ -1,13 +1,14 @@
 package com.picpay.desafio.android.repository
 
-import com.picpay.desafio.android.api.PicPayServiceApi
+import com.picpay.desafio.android.api.interfaces.PicPayServiceApiInterface
 import com.picpay.desafio.provider.model.User
 import com.picpay.desafio.service.RestApi
 import retrofit2.Call
 
 class PicPayRepository {
 
-    val api: PicPayServiceApi = RestApi.getRetrofit().create(PicPayServiceApi::class.java)
+    val api: PicPayServiceApiInterface = RestApi.getRetrofit().create(
+        PicPayServiceApiInterface::class.java)
 
     fun getUsers(): Call<List<User>> {
         return api.getUsers()

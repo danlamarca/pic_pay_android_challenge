2de4115968597e919d312ce624c3a9f7501ca8f6
Danilo - Micro de Desenv <lamarca.danilo@gmail.com>
2022-06-10 08:57:24 -0300

    inicio da utilizacao de usecase

--- app/src/main/java/com/picpay/desafio/android/domain/usecase/UserUseCaseDomain.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/usecase/UserUseCaseDomain.kt
@@ -1,5 +1,9 @@
 package com.picpay.desafio.android.domain.usecase
 
-class UserUseCaseDomain {
+import com.picpay.desafio.android.data.model.User
 
+class UserUseCaseDomain(private val users: List<User>) {
+    suspend operator fun invoke(): List<User>{
+        return this.users.sortedBy { it.comparator() }
+    }
 }
--- app/src/main/java/com/picpay/desafio/android/domain/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/picpay/desafio/android/domain/viewmodel/UserViewModel.kt
@@ -3,6 +3,7 @@ package com.picpay.desafio.android.domain.viewmodel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.picpay.desafio.android.data.repository.UserRepository
+import com.picpay.desafio.android.domain.usecase.UserUseCaseDomain
 import com.picpay.desafio.service.Utils.Resource
 import kotlinx.coroutines.Dispatchers
 
@@ -10,7 +11,7 @@ class UserViewModel(private val userRepository: UserRepository): ViewModel() {
     fun getUsers() = liveData(Dispatchers.IO){
         emit(Resource.loading(data=null))
         try{
-            emit(Resource.success(data = userRepository.getUsers()))
+            emit(Resource.success(data = UserUseCaseDomain(userRepository.getUsers()).invoke()))
         }catch (ex: Exception){
             emit(Resource.error(data = null, message = "An error has ocurred: ${ex.message})"))
         }
